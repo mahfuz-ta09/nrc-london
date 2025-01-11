1566c188126a71011613a462c2bbfdf66e356aeb
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-01-12 01:10:25 +0600

    process added

--- src/app/(withnavbar)/page.tsx
+++ src/app/(withnavbar)/page.tsx
@@ -5,6 +5,7 @@ import PopularCourse from "@/component/shared/PopularCourse/PopularCourse"
 import AcademicSolution from "@/component/shared/AcademicSolution/AcademicSolution"
 import Review from "@/component/additional/Review/Review"
 import FAQ from "@/component/additional/FAQ/FAQ"
+import Process from "@/component/shared/Process/Process"
 
 
 const Page = () => {
@@ -14,6 +15,7 @@ const Page = () => {
         <div className="home-contain">
           <PopularCourse />
           <UniOption/>
+          <Process />
           <AcademicSolution />
           <Review />
           <FAQ />
