b418e19656155daf4f542604b4d55fa651ea3120
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-01-16 23:25:20 +0600

    faq->reusable component 1.1

--- src/app/(withnavbar)/page.tsx
+++ src/app/(withnavbar)/page.tsx
@@ -4,11 +4,26 @@ import UniOption from "@/component/shared/UniOption/UniOption"
 import PopularCourse from "@/component/shared/PopularCourse/PopularCourse"
 import AcademicSolution from "@/component/shared/AcademicSolution/AcademicSolution"
 import Review from "@/component/additional/Review/Review"
-import FAQ from "@/component/additional/FAQ/FAQ"
 import Process from "@/component/shared/Process/Process"
+import FAQ from "@/component/UI/FAQ/FAQ"
 
 
 const Page = () => {
+  const faqItems = [
+    {
+      question: "What is the meaning of life?",
+      answer: "42",
+    },
+    {
+      question: "How much wood would a woodchuck chuck?",
+      answer: "A woodchuck would chuck all the wood he could chuck, if a woodchuck could chuck wood!",
+    },
+    {
+      question: "What happens if Pinocchio says, 'my nose will grow now'?",
+      answer: "This is a paradox that has puzzled philosophers and fairy tale enthusiasts alike!",
+    },
+  ]
+  
   return (
     <>
         <Banner />
@@ -18,7 +33,7 @@ const Page = () => {
           <Process />
           <AcademicSolution />
           <Review />
-          <FAQ />
+          <FAQ items={faqItems}  title="Frequently Asked Questions"/>
         </div>
         <Footer />
     </>
