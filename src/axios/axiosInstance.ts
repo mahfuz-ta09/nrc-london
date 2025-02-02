8a1742f985013be0031b12a5eacbec5ff91dfa0d
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-02-03 03:58:56 +0600

    axios and redux added1.5

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -1,40 +1,43 @@
-import { responseSuccess } from "@/types/common"
-import axios from "axios"
+import { responseError, responseSuccess } from '@/types/common'
+import axios from 'axios'
+
 
 const instance = axios.create()
-instance.defaults.headers.post['Content-Type']='application/json'
-instance.defaults.headers['Accept']='application/json'
-instance.defaults.timeout=60000
+instance.defaults.headers.post["Content-Type"] = "application/json"
+instance.defaults.headers["Accept"] = "application/json"
+instance.defaults.timeout = 60000
 
 
-instance.interceptors.request.use(
-    function (config) {
-        console.log(config)    
-
-    return config;
-},
-function(error){
+instance.interceptors.request.use(function (config) {
     
+    return config
+  }, function (error) {
 
-    return Promise.reject(error);
+
+    return Promise.reject(error)
 })
 
 
-
 instance.interceptors.response.use(
-// @ts-expect-error:TypeScript might not infer the correct type for response
+//@ts-ignore
 
-    function (response){
-        
-        const responseObject:responseSuccess = {
-            data: response?.data?.data,
-            meta: response?.data?.meta,
-        }
-  
-      return responseObject
-},
-function (error){
-    return Promise.reject(error)
+function (response) {
+
+    const responseObject:responseSuccess = {
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    }
+
+    return responseObject
+}, function (error) {
+
+    const responseObject:responseError = {
+      errorMessage: error?.response.data?.message,
+      statusCode: error?.response?.data?.statusCode || 500,
+      message: error?.response.data?.message || "something went wrong!!",
+    }
+
+    return responseObject
 })
 
 export { instance }
