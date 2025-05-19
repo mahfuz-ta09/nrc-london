9d9a527932c61018c1fd611429a8180d372d05df
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-05-20 01:37:44 +0600

    --1

--- src/component/shared/Banner/Banner.tsx
+++ src/component/shared/Banner/Banner.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../../css/shared/Banner/Banner.css'
-import { faArrowAltCircleRight, faCalendarDays, faGraduationCap, faPlaneDeparture, faSchool } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays, faGraduationCap, faPlaneDeparture, faSchool } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/navigation'
 import imgBgBanner from '../../../assets/banner-background.png'
-import BannerImg from '../../../assets/image-banner.png'
 import Image from 'next/image'
 
 
@@ -21,44 +20,44 @@ const Banner = () => {
                     <p>Empower yourself with knowledge and build a brighter future. Our comprehensive courses and expert support will guide you every step of the way.</p>
                     <div className="banner-buttons">
                         <button onClick={()=>router.push('/Proceed')} className='banner-button-3rd'>I am a Student</button>
-                        <button onClick={()=>router.push('/RecruitmentPartner/Becomeanagent')} className='banner-button-3rd'>become an agent</button>
+                        <button onClick={()=>router.push('/RecruitmentPartner/Becomeanagent')} className='banner-button-3rd'>Recruitment Partner</button>
                     </div>
                 </div>
                 {/* <Image className="banner-photo" src={BannerImg} alt="Banner" /> */}
-                <div className="banner-addition">
-                    <div className="achiev">
-                        <FontAwesomeIcon className='achiev-photo' icon={faPlaneDeparture}/>
-                        <div className="achiev-details">
-                            <h1>30+</h1>
-                            <p>Study</p>
-                            <p>Destination</p>
-                        </div>
-                    </div>
-                    <div className="achiev">
-                        <FontAwesomeIcon className='achiev-photo' icon={faSchool}/>
-                        <div className="achiev-details">
-                            <h1>70+</h1>
-                            <p>Global</p>
-                            <p>Institutions</p>
-                        </div>
-                    </div>
-                    <div className="achiev">
-                        <FontAwesomeIcon className='achiev-photo' icon={faCalendarDays} />
-                        <div className="achiev-details">
-                            <h1>24/7</h1>
-                            <p>Online</p>
-                            <p>Presence</p>
-                        </div>
-                    </div>
-                    <div className="achiev">
-                        <FontAwesomeIcon className='achiev-photo' icon={faGraduationCap} />
-                        <div className="achiev-details">
-                            <h1>500+</h1>
-                            <p>Students</p>
-                            <p>Consulted</p>
-                        </div>
+            <div className="banner-addition">
+                <div className="achiev">
+                    <FontAwesomeIcon className='achiev-photo' icon={faPlaneDeparture}/>
+                    <div className="achiev-details">
+                        <h1>30+</h1>
+                        <p>Study</p>
+                        <p>Destination</p>
                     </div>
                 </div>
+                <div className="achiev">
+                    <FontAwesomeIcon className='achiev-photo' icon={faSchool}/>
+                    <div className="achiev-details">
+                        <h1>70+</h1>
+                        <p>Global</p>
+                        <p>Institutions</p>
+                    </div>
+                </div>
+                <div className="achiev">
+                    <FontAwesomeIcon className='achiev-photo' icon={faCalendarDays} />
+                    <div className="achiev-details">
+                        <h1>24/7</h1>
+                        <p>Online</p>
+                        <p>Presence</p>
+                    </div>
+                </div>
+                <div className="achiev">
+                    <FontAwesomeIcon className='achiev-photo' icon={faGraduationCap} />
+                    <div className="achiev-details">
+                        <h1>500+</h1>
+                        <p>Students</p>
+                        <p>Consulted</p>
+                    </div>
+                </div>
+            </div>
             </div>
 
 
