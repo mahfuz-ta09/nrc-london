87813a97c3a6e42447657be6b05efbb2690a6cd7
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-05-19 18:13:10 +0600

    update api

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 
 let url = ''
 url = 'https://nrc-server-production-19f8.up.railway.app/app/v1'
-// url = 'http://localhost:7373/app/v1'
+url = 'http://localhost:7373/app/v1'
 
 
 
