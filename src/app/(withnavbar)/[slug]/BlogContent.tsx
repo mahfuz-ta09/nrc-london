fada660944894c712b89760db888d19d9a1ceb5a
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2025-10-09 03:25:05 +0600

    as

--- src/app/(withnavbar)/[slug]/BlogContent.tsx
+++ src/app/(withnavbar)/[slug]/BlogContent.tsx
@@ -14,7 +14,6 @@ interface BlogContentProps {
 export default function BlogContent({ data }: BlogContentProps) {
 
     return (
-      <>
         <div className="page-container">
           <div className="page-banner">
             <div className="banner-content">
@@ -24,47 +23,36 @@ export default function BlogContent({ data }: BlogContentProps) {
                   <Link className='bread-link2'  href="">details blog</Link>
               
               </div>
-              {data?.meta?.ogImage?.url && (
-                <img
-                  src={data?.meta?.ogImage?.url}
-                  alt="blog header image"
-                  className="banner-image"
-                />
-              )}
 
             </div>
           </div>
           
-
-            <div className="blog-detail">
-              <h1>{data?.title}</h1>
-              <p>{data?.publishedAt} By {data?.author}</p>
-              
-              <div className="like-dislike">
-                <h5>{data?.stats?.views}<FontAwesomeIcon className="like-icon" icon={faEye}/></h5>
-                <h5>{data?.stats?.likes}<FontAwesomeIcon className="like-icon" icon={faThumbsUp}/></h5>
-              </div>
-              
               <div className="blog-detail-container">
                 
                 <div className="blog-body">
                   <div className="blog-detail-container-body">
-                    <h4>{data?.content?.summary}</h4>
-                    <div
-                      dangerouslySetInnerHTML={{ __html: data?.content || "" }}
-                    />
+                      {data?.meta?.ogImage?.url && (
+                        <img
+                          src={data?.meta?.ogImage?.url}
+                          alt="blog header image"
+                          className="body-image"
+                        /> )}
+
+                      <h1>{data?.title}</h1>
+                      <p>{data?.publishedAt}</p>
+                      <h4>{data?.content?.summary}</h4>
+                      
+                      <div dangerouslySetInnerHTML={{ __html: data?.content || "" }}/>
                   </div>
                 </div>
-                <div className="blog-form">
+                <div className="blog-info-part">
                   <Form />
                   <AcademicSolution/>
                 </div>
               </div>
-          </div>
-
-        </div>
 
         <Footer />
-      </>
+        </div>
+
     )
 }
